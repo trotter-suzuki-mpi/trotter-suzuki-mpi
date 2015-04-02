de1b64da8206a4d9bd89f3b806cc6de297ef0102
Luca Calderaro <calderaroluca@gmail.com>
2015-04-02 11:24:02 +0200

    Astyle: --style=banner --pad=oper

--- src/trottertest.cpp
+++ src/trottertest.cpp
@@ -37,16 +37,15 @@
 #define KERNEL_TYPE 0
 #define SNAPSHOTS 0
 
-void trotter(const int matrix_width, const int matrix_height, const int iterations, const int snapshots, const int kernel_type)
-{
+void trotter(const int matrix_width, const int matrix_height, const int iterations, const int snapshots, const int kernel_type) {
 
     float * p_real, * p_imag;
     std::stringstream filename;
     int start_x, end_x, inner_start_x, inner_end_x,
         start_y, end_y, inner_start_y, inner_end_y;
     MPI_Comm cartcomm;
-    int coords[2], dims[2]= {0,0};
-    int periods[2]= {0, 0};
+    int coords[2], dims[2] = {0, 0};
+    int periods[2] = {0, 0};
     int rank;
     int nProcs;
 
@@ -56,17 +55,17 @@ void trotter(const int matrix_width, const int matrix_height, const int iteratio
     MPI_Comm_rank(cartcomm, &rank);
     MPI_Cart_coords(cartcomm, rank, 2, coords); //Determines process coords in cartesian topology given rank in group
 
-    int halo_x=(kernel_type==2 ? 3 : 4);
-    int halo_y=4;
+    int halo_x = (kernel_type == 2 ? 3 : 4);
+    int halo_y = 4;
     calculate_borders(coords[1], dims[1], &start_x, &end_x, &inner_start_x, &inner_end_x, matrix_width, halo_x);
     calculate_borders(coords[0], dims[0], &start_y, &end_y, &inner_start_y, &inner_end_y, matrix_height, halo_y);
-    int width=end_x-start_x;
-    int height=end_y-start_y;
+    int width = end_x - start_x;
+    int height = end_y - start_y;
 #ifdef DEBUG
-    std::cout << "Coord_x: "<< coords[1] << " start_x: " << start_x << \
-              " end_x: "<< end_x << " inner_start_x " << inner_start_x << " inner_end_x " << inner_end_x << "\n";
-    std::cout << "Coord_y: "<< coords[0] << " start_y: " << start_y << \
-              " end_y: "<< end_y << " inner_start_y " << inner_start_y << " inner_end_y " << inner_end_y<< "\n";
+    std::cout << "Coord_x: " << coords[1] << " start_x: " << start_x << \
+              " end_x: " << end_x << " inner_start_x " << inner_start_x << " inner_end_x " << inner_end_x << "\n";
+    std::cout << "Coord_y: " << coords[0] << " start_y: " << start_y << \
+              " end_y: " << end_y << " inner_start_y " << inner_start_y << " inner_end_y " << inner_end_y << "\n";
 #endif
 
     // Allocate and initialize matrices
@@ -76,22 +75,20 @@ void trotter(const int matrix_width, const int matrix_height, const int iteratio
 
     // Initialize kernel
     ITrotterKernel * kernel;
-    switch (kernel_type)
-    {
+    switch (kernel_type) {
     case 0:
-        kernel=new CPUBlock(p_real, p_imag, h_a, h_b, matrix_width, matrix_height, halo_x, halo_y, cartcomm);
+        kernel = new CPUBlock(p_real, p_imag, h_a, h_b, matrix_width, matrix_height, halo_x, halo_y, cartcomm);
         break;
 
     case 1:
-        kernel=new CPUBlockSSEKernel(p_real, p_imag, h_a, h_b, matrix_width, matrix_height, halo_x, halo_y, cartcomm);
+        kernel = new CPUBlockSSEKernel(p_real, p_imag, h_a, h_b, matrix_width, matrix_height, halo_x, halo_y, cartcomm);
         break;
 
     case 2:
 #ifdef CUDA
-        kernel=new CC2Kernel(p_real, p_imag, h_a, h_b, matrix_width, matrix_height, halo_x, halo_y, cartcomm);
+        kernel = new CC2Kernel(p_real, p_imag, h_a, h_b, matrix_width, matrix_height, halo_x, halo_y, cartcomm);
 #else
-        if (coords[0]==0 && coords[1]==0)
-        {
+        if (coords[0] == 0 && coords[1] == 0) {
             std::cerr << "Compiled without CUDA\n";
         }
         MPI_Abort(MPI_COMM_WORLD, 2);
@@ -100,10 +97,9 @@ void trotter(const int matrix_width, const int matrix_height, const int iteratio
 
     case 3:
 #ifdef CUDA
-        kernel=new HybridKernel(p_real, p_imag, h_a, h_b, matrix_width, matrix_height, halo_x, halo_y, cartcomm);
+        kernel = new HybridKernel(p_real, p_imag, h_a, h_b, matrix_width, matrix_height, halo_x, halo_y, cartcomm);
 #else
-        if (coords[0]==0 && coords[1]==0)
-        {
+        if (coords[0] == 0 && coords[1] == 0) {
             std::cerr << "Compiled without CUDA\n";
         }
         MPI_Abort(MPI_COMM_WORLD, 2);
@@ -111,7 +107,7 @@ void trotter(const int matrix_width, const int matrix_height, const int iteratio
         break;
 
     default:
-        kernel=new CPUBlock(p_real, p_imag, h_a, h_b, matrix_width, matrix_height, halo_x, halo_y, cartcomm);
+        kernel = new CPUBlock(p_real, p_imag, h_a, h_b, matrix_width, matrix_height, halo_x, halo_y, cartcomm);
         break;
     }
 
@@ -119,42 +115,36 @@ void trotter(const int matrix_width, const int matrix_height, const int iteratio
     gettimeofday(&start, NULL);
 
     // Main loop
-    for (int i = 0; i < iterations; i++)
-    {
-        if ( (snapshots>0) && (i % snapshots == 0) )
-        {
+    for (int i = 0; i < iterations; i++) {
+        if ( (snapshots > 0) && (i % snapshots == 0) ) {
             kernel->get_sample(width, 0, 0, width, height, p_real, p_imag);
             filename.str("");
             filename << i << "-iter-" << coords[1] << "-" << coords[0] << "-real.dat";
-            print_matrix(filename.str(), p_real+((inner_start_y-start_y)*width+inner_start_x-start_x),
-                         width, inner_end_x-inner_start_x, inner_end_y-inner_start_y);
+            print_matrix(filename.str(), p_real + ((inner_start_y - start_y)*width + inner_start_x - start_x),
+                         width, inner_end_x - inner_start_x, inner_end_y - inner_start_y);
         }
         kernel->run_kernel_on_halo();
-        if (i!=iterations-1)
-        {
+        if (i != iterations - 1) {
             kernel->start_halo_exchange();
         }
         kernel->run_kernel();
-        if (i!=iterations-1)
-        {
+        if (i != iterations - 1) {
             kernel->finish_halo_exchange();
         }
         kernel->wait_for_completion();
     }
 
     gettimeofday(&end, NULL);
-    if (coords[0]==0 && coords[1]==0)
-    {
+    if (coords[0] == 0 && coords[1] == 0) {
         long time = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec);
-        std::cout << "TROTTER " << matrix_width << "x" << matrix_height << " " << kernel->get_name() << " " << nProcs << " " <<time << std::endl;
+        std::cout << "TROTTER " << matrix_width << "x" << matrix_height << " " << kernel->get_name() << " " << nProcs << " " << time << std::endl;
     }
     delete[] p_real;
     delete[] p_imag;
     delete kernel;
 }
 
-void print_usage()
-{
+void print_usage() {
     std::cout << "Usage:\n" \
               "     trottertest [OPTION]\n" \
               "Arguments:\n" \
@@ -169,31 +159,26 @@ void print_usage()
               "                   Zero means no snapshots. Default: " << SNAPSHOTS << ".\n";
 }
 
-void process_command_line(int argc, char** argv, int *dim, int *iterations, int *snapshots, int *kernel_type)
-{
+void process_command_line(int argc, char** argv, int *dim, int *iterations, int *snapshots, int *kernel_type) {
     // Setting default values
-    *dim=DIM;
-    *iterations=ITERATIONS;
-    *snapshots=SNAPSHOTS;
-    *kernel_type=KERNEL_TYPE;
+    *dim = DIM;
+    *iterations = ITERATIONS;
+    *snapshots = SNAPSHOTS;
+    *kernel_type = KERNEL_TYPE;
 
     int c;
-    while ((c = getopt (argc, argv, "d:hi:k:s:")) != -1)
-    {
-        switch (c)
-        {
+    while ((c = getopt (argc, argv, "d:hi:k:s:")) != -1) {
+        switch (c) {
         case 'd':
             *dim = atoi(optarg);
-            if (*dim<=0)
-            {
+            if (*dim <= 0) {
                 fprintf (stderr, "The argument of option -d should be a positive integer.\n");
                 MPI_Abort(MPI_COMM_WORLD, 1);
             }
             break;
         case 'i':
             *iterations = atoi(optarg);
-            if (*iterations<=0)
-            {
+            if (*iterations <= 0) {
                 fprintf (stderr, "The argument of option -i should be a positive integer.\n");
                 MPI_Abort(MPI_COMM_WORLD, 1);
             }
@@ -204,35 +189,30 @@ void process_command_line(int argc, char** argv, int *dim, int *iterations, int 
             break;
         case 'k':
             *kernel_type = atoi(optarg);
-            if (*kernel_type<0||*kernel_type>3)
-            {
+            if (*kernel_type < 0 || *kernel_type > 3) {
                 fprintf (stderr, "The argument of option -k should be a valid kernel.\n");
                 MPI_Abort(MPI_COMM_WORLD, 1);
             }
             break;
         case 's':
             *snapshots = atoi(optarg);
-            if (*snapshots<=0)
-            {
+            if (*snapshots <= 0) {
                 fprintf (stderr, "The argument of option -s should be a positive integer.\n");
                 MPI_Abort(MPI_COMM_WORLD, 1);
             }
             break;
         case '?':
-            if (optopt == 'd' || optopt == 'i' || optopt == 'k' || optopt == 's')
-            {
+            if (optopt == 'd' || optopt == 'i' || optopt == 'k' || optopt == 's') {
                 fprintf (stderr, "Option -%c requires an argument.\n", optopt);
                 print_usage();
                 MPI_Abort(MPI_COMM_WORLD, 1);
             }
-            else if (isprint (optopt))
-            {
+            else if (isprint (optopt)) {
                 fprintf (stderr, "Unknown option `-%c'.\n", optopt);
                 print_usage();
                 MPI_Abort(MPI_COMM_WORLD, 1);
             }
-            else
-            {
+            else {
                 fprintf (stderr, "Unknown option character `\\x%x'.\n",  optopt);
                 print_usage();
                 MPI_Abort(MPI_COMM_WORLD, 1);
@@ -243,15 +223,13 @@ void process_command_line(int argc, char** argv, int *dim, int *iterations, int 
     }
 }
 
-int main(int argc, char** argv)
-{
-    int dim=0, iterations=0, snapshots=0, kernel_type=0;
+int main(int argc, char** argv) {
+    int dim = 0, iterations = 0, snapshots = 0, kernel_type = 0;
     int rank;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    if (rank==0)
-    {
+    if (rank == 0) {
         process_command_line(argc, argv, &dim, &iterations, &snapshots, &kernel_type);
     }
     MPI_Bcast(&dim, 1, MPI_INT, 0, MPI_COMM_WORLD);
