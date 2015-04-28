796457c65142e81485792f2a1143d3a79446355d
Luca Calderaro <calderaroluca@gmail.com>
2015-04-28 16:59:11 +0200

    extern potential + expect values

--- examples/particle_in_a_box.cpp
+++ examples/particle_in_a_box.cpp
@@ -17,27 +17,65 @@
  *
  */
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string>
+#include <sstream>
+
 #include <fstream>
 #include <unistd.h>
 #include <stdlib.h>
 #include <iostream>
 #include <complex>
-#include "trotter.h" 
+
+#include "trotter.h"
+#include "expect.h"
 
 #define DIM 640
 #define ITERATIONS 1000
 #define KERNEL_TYPE 0
 #define SNAPSHOTS 0
 
-void init_p(float *p_real, float *p_imag, int dimx, int dimy) {
+//external potential operator in coordinate representation
+void potential_op_coord_representation(float *hamilt_pot, int dimx, int dimy) {
+    float constant = 1.;
+    for(int i = 0; i < dimy; i++) {
+        for(int j = 0; j < dimx; j++) {
+            hamilt_pot[i * dimx + j] = constant;
+        }
+    }
+}
+
+void init_state(float *p_real, float *p_imag, int dimx, int dimy) {
     double s = 64.0; // FIXME: y esto?
+    double L_x = dimx;
+    double L_y = dimy;
+    double n_x = 1., n_y = 1.;
+
     for (int y = 1; y <= dimy; y++) {
         for (int x = 1; x <= dimx; x++) {
-            std::complex<float> tmp = std::complex<float>(exp(-(pow(x - 180.0, 2.0) + pow(y - 300.0, 2.0)) / (2.0 * pow(s, 2.0))), 0.0)
-                                      * exp(std::complex<float>(0.0, 0.4 * (x + y - 480.0)));
+            //std::complex<float> tmp = std::complex<float>(exp(-(pow(x - 180.0, 2.0) + pow(y - 300.0, 2.0)) / (2.0 * pow(s, 2.0))), 0.0)
+            //                        * exp(std::complex<float>(0.0, 0.4 * (x + y - 480.0)));
+
+            std::complex<float> tmp = std::complex<float> (sin(2 * 3.14159 / L_x * x) * sin(2 * 3.14159 / L_y * y), 0.0);
 
             p_real[y * dimx + x] = real(tmp);
             p_imag[y * dimx + x] = imag(tmp);
+        }
+    }
+}
+
+//calculate potential part of evolution operator
+void init_pot_evolution_op(float * hamilt_pot, float * external_pot_real, float * external_pot_imag, int dimx, int dimy, double particle_mass, double time_single_it ) {
+    float CONST_1 = -1. * time_single_it;
+    float CONST_2 = 2. * time_single_it / particle_mass;		//CONST_2: discretization of momentum operator and the only effect is to produce a scalar operator, so it could be omitted
+
+    std::complex<float> tmp;
+    for(int i = 0; i < dimy; i++) {
+        for(int j = 0; j < dimx; j++) {
+            tmp = exp(std::complex<float> (0., CONST_1 * hamilt_pot[i * dimx + j] + CONST_2));
+            external_pot_real[i * dimx + j] = real(tmp);
+            external_pot_imag[i * dimx + j] = imag(tmp);
         }
     }
 }
@@ -54,10 +92,12 @@ void print_usage() {
               "                      2: GPU\n" \
               "                      3: Hybrid (experimental) \n" \
               "     -s NUMBER     Snapshots are taken at every NUMBER of iterations.\n" \
-              "                   Zero means no snapshots. Default: " << SNAPSHOTS << ".\n";
+              "                   Zero means no snapshots. Default: " << SNAPSHOTS << ".\n"\
+              "     -v            Calculates expected values of energy and momentum operators\n"\
+              "                   once the simulation is finished";
 }
 
-void process_command_line(int argc, char** argv, int *dim, int *iterations, int *snapshots, int *kernel_type) {
+void process_command_line(int argc, char** argv, int *dim, int *iterations, int *snapshots, int *kernel_type, bool *values) {
     // Setting default values
     *dim = DIM;
     *iterations = ITERATIONS;
@@ -65,7 +105,7 @@ void process_command_line(int argc, char** argv, int *dim, int *iterations, int 
     *kernel_type = KERNEL_TYPE;
 
     int c;
-    while ((c = getopt (argc, argv, "d:hi:k:s:")) != -1) {
+    while ((c = getopt (argc, argv, "d:hi:k:s:v")) != -1) {
         switch (c) {
         case 'd':
             *dim = atoi(optarg);
@@ -99,6 +139,9 @@ void process_command_line(int argc, char** argv, int *dim, int *iterations, int 
                 abort ();
             }
             break;
+        case 'v':
+            *values = true;
+            break;
         case '?':
             if (optopt == 'd' || optopt == 'i' || optopt == 'k' || optopt == 's') {
                 fprintf (stderr, "Option -%c requires an argument.\n", optopt);
@@ -121,17 +164,52 @@ void process_command_line(int argc, char** argv, int *dim, int *iterations, int 
     }
 }
 
-int main(int argc, char** argv){
-	int dim = 0, iterations = 0, snapshots = 0, kernel_type = 0;
-	float * p_real;
-	float * p_imag;
-   
-	process_command_line(argc, argv, &dim, &iterations, &snapshots, &kernel_type);
-	p_real = new float[dim*dim];
-	p_imag = new float[dim*dim];
-	
-	init_p(p_real, p_imag, dim, dim);
-	trotter(p_real, p_imag, dim, dim, iterations, snapshots, kernel_type, argc, argv);
+int main(int argc, char** argv) {
+    int dim = 0, iterations = 0, snapshots = 0, kernel_type = 0;
+    bool values = false;
+
+    process_command_line(argc, argv, &dim, &iterations, &snapshots, &kernel_type, &values);
+
+    //set hamiltonian variables
+    const double particle_mass = 1.;
+    float *hamilt_pot = new float[dim * dim];
+    potential_op_coord_representation(hamilt_pot, dim, dim);	//set potential operator
+
+    //set and calculate evolution operator variables from hamiltonian
+    const double time_single_it = 0.08 * particle_mass / 2.;	//second approx trotter-suzuki: time/2
+    float *external_pot_real = new float[dim * dim];
+    float *external_pot_imag = new float[dim * dim];
+    init_pot_evolution_op(hamilt_pot, external_pot_real, external_pot_imag, dim, dim, particle_mass, time_single_it);	//calculate potential part of evolution operator
+    static const double h_a = cos(time_single_it / (2. * particle_mass));
+    static const double h_b = sin(time_single_it / (2. * particle_mass));
+
+    //set initial state
+    float *p_real = new float[dim * dim];
+    float *p_imag = new float[dim * dim];
+    init_state(p_real, p_imag, dim, dim);
+
+    //set file output directory
+    std::stringstream filename;
+    std::string filenames;
+    if(snapshots) {
+        int status;
+
+        filename.str("");
+        filename << "D" << dim << "_I" << iterations << "_S" << snapshots << "";
+        filenames = filename.str();
+
+        status = mkdir(filenames.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+
+        if(status != 0 && status != -1)
+            filenames = "./";
+    }
+    else
+        filenames = "./";
+
+    trotter(h_a, h_b, external_pot_real, external_pot_imag, p_real, p_imag, dim, dim, iterations, snapshots, kernel_type, argc, argv, filenames.c_str());
+
+    if(values)
+        expect_values(dim, iterations, snapshots, hamilt_pot, particle_mass, filenames.c_str());
 
     return 0;
 }
