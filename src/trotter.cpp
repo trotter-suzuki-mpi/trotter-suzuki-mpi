6d3e990f3cfdbb98a8de96639befd4c50f284762
Luca Calderaro <calderaroluca@gmail.com>
2015-04-28 17:01:45 +0200

    extern potential + expect values

--- src/trotter.cpp
+++ src/trotter.cpp
@@ -34,19 +34,20 @@
 #include "hybrid.h"
 #endif
 
-void trotter(float * p_real, float * p_imag, const int matrix_width, const int matrix_height, const int iterations, const int snapshots, const int kernel_type, int argc, char** argv) {
-	
-	float *_p_real, *_p_imag;
+void trotter(double h_a, double h_b, float * external_pot_real, float * external_pot_imag, float * p_real, float * p_imag, const int matrix_width, const int matrix_height, const int iterations, const int snapshots, const int kernel_type, int argc, char** argv, const char *dirname) {
+
+    float *_p_real, *_p_imag;
+    float *_external_pot_real, *_external_pot_imag;
     std::stringstream filename;
     int start_x, end_x, inner_start_x, inner_end_x,
         start_y, end_y, inner_start_y, inner_end_y;
-    
+
     int coords[2], dims[2] = {0, 0};
     int periods[2] = {0, 0};
     int rank;
     int nProcs;
 
-	MPI_Init(&argc, &argv);
+    MPI_Init(&argc, &argv);
     //MPI_Bcast(&matrix_width, 1, MPI_INT, 0, MPI_COMM_WORLD);
     //MPI_Bcast(&iterations, 1, MPI_INT, 0, MPI_COMM_WORLD);
     //MPI_Bcast(&snapshots, 1, MPI_INT, 0, MPI_COMM_WORLD);
@@ -57,7 +58,7 @@ void trotter(float * p_real, float * p_imag, const int matrix_width, const int m
     MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, 0, &cartcomm);
     MPI_Comm_rank(cartcomm, &rank);
     MPI_Cart_coords(cartcomm, rank, 2, coords); //Determines process coords in cartesian topology given rank in group
-	
+
     int halo_x = (kernel_type == 2 ? 3 : 4);
     int halo_y = 4;
     calculate_borders(coords[1], dims[1], &start_x, &end_x, &inner_start_x, &inner_end_x, matrix_width, halo_x);
@@ -71,20 +72,27 @@ void trotter(float * p_real, float * p_imag, const int matrix_width, const int m
               " end_y: " << end_y << " inner_start_y " << inner_start_y << " inner_end_y " << inner_end_y << "\n";
 #endif
 
-    // Allocate and initialize matrices
+    // Allocate matrices
     _p_real = new float[width * height];
     _p_imag = new float[width * height];
-    	
-    memcpy2D(_p_real, width * sizeof(float), &p_real[start_y*matrix_width + start_x], matrix_width * sizeof(float), width * sizeof(float), height);
-	memcpy2D(_p_imag, width * sizeof(float), &p_imag[start_y*matrix_width + start_x], matrix_width * sizeof(float), width * sizeof(float), height);
-	delete[] p_real;
-	delete[] p_imag;
-	
+    _external_pot_real = new float[width * height];
+    _external_pot_imag = new float[width * height];
+
+    memcpy2D(_p_real, width * sizeof(float), &p_real[start_y * matrix_width + start_x], matrix_width * sizeof(float), width * sizeof(float), height);
+    memcpy2D(_p_imag, width * sizeof(float), &p_imag[start_y * matrix_width + start_x], matrix_width * sizeof(float), width * sizeof(float), height);
+    memcpy2D(_external_pot_real, width * sizeof(float), &external_pot_real[start_y * matrix_width + start_x], matrix_width * sizeof(float), width * sizeof(float), height);
+    memcpy2D(_external_pot_imag, width * sizeof(float), &external_pot_imag[start_y * matrix_width + start_x], matrix_width * sizeof(float), width * sizeof(float), height);
+
+    delete[] external_pot_real;
+    delete[] external_pot_imag;
+    delete[] p_real;
+    delete[] p_imag;
+
     // Initialize kernel
     ITrotterKernel * kernel;
     switch (kernel_type) {
     case 0:
-        kernel = new CPUBlock(_p_real, _p_imag, h_a, h_b, matrix_width, matrix_height, halo_x, halo_y, cartcomm);
+        kernel = new CPUBlock(_p_real, _p_imag, _external_pot_real, _external_pot_imag, h_a, h_b, matrix_width, matrix_height, halo_x, halo_y, cartcomm);
         break;
 
     case 1:
@@ -114,7 +122,7 @@ void trotter(float * p_real, float * p_imag, const int matrix_width, const int m
         break;
 
     default:
-        kernel = new CPUBlock(_p_real, _p_imag, h_a, h_b, matrix_width, matrix_height, halo_x, halo_y, cartcomm);
+        kernel = new CPUBlock(_p_real, _p_imag, _external_pot_real, _external_pot_imag, h_a, h_b, matrix_width, matrix_height, halo_x, halo_y, cartcomm);
         break;
     }
 
@@ -126,14 +134,14 @@ void trotter(float * p_real, float * p_imag, const int matrix_width, const int m
         if ( (snapshots > 0) && (i % snapshots == 0) ) {
             kernel->get_sample(width, 0, 0, width, height, _p_real, _p_imag);
             filename.str("");
-            filename << i << "-iter-" << coords[1] << "-" << coords[0] << "-real.dat";
+            filename << dirname << "/" << i << "-iter-" << coords[1] << "-" << coords[0] << "-real.dat";
             print_matrix(filename.str(), _p_real + ((inner_start_y - start_y)*width + inner_start_x - start_x),
                          width, inner_end_x - inner_start_x, inner_end_y - inner_start_y);
-            
+
             filename.str("");
-            filename << i << "-iter-" << coords[1] << "-" << coords[0] << "-comp.dat";
+            filename << dirname << "/" << i << "-iter-" << coords[1] << "-" << coords[0] << "-comp.dat";
             print_complex_matrix(filename.str(), _p_real + ((inner_start_y - start_y)*width + inner_start_x - start_x),
-						 _p_imag + ((inner_start_y - start_y)*width + inner_start_x - start_x), width, inner_end_x - inner_start_x, inner_end_y - inner_start_y);
+                                 _p_imag + ((inner_start_y - start_y)*width + inner_start_x - start_x), width, inner_end_x - inner_start_x, inner_end_y - inner_start_y);
         }
         kernel->run_kernel_on_halo();
         if (i != iterations - 1) {
@@ -154,6 +162,6 @@ void trotter(float * p_real, float * p_imag, const int matrix_width, const int m
     delete[] _p_real;
     delete[] _p_imag;
     delete kernel;
-    
+
     MPI_Finalize();
 }
