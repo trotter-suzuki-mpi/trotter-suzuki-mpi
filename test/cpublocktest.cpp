6507d39f36e2a0f39247c599218874320fd41628
Luca Calderaro <calderaroluca@gmail.com>
2015-04-28 17:13:00 +0200

    extern potential + expect values

--- test/cpublocktest.cpp
+++ test/cpublocktest.cpp
@@ -25,8 +25,8 @@
 
 CPPUNIT_TEST_SUITE_REGISTRATION( CPUBlockTest );
 
-void CPUBlockTest::setUp(){}
-void CPUBlockTest::tearDown(){}
+void CPUBlockTest::setUp() {}
+void CPUBlockTest::tearDown() {}
 
 //cpublocksse's functions
 template <int offset_y>
@@ -116,227 +116,227 @@ inline void update_shiftx_sse(size_t stride, size_t width, size_t height, float 
 }
 
 
-void CPUBlockTest::test_block_kernel_vertical(){
-	//Set Up
-	int DIM=640;
-	int offset=0;
-	float a=h_a, b=h_b;
-	
-	float *block_real = new float[DIM*DIM];
-    float *block_imag = new float[DIM*DIM];
-    float *block_real_expected = new float[DIM*DIM];
-    float *block_imag_expected = new float[DIM*DIM];
-    
-    //initialize block_real, block_imag
-    for(int i=0; i<DIM; i++){
-		for(int j=0; j<DIM; j++){
-			block_real[i*DIM+j] = 1.;
-			block_imag[i*DIM+j] = 0.;
-		}
-	}
-	
-	//inizialize block_real_expected, block_imag_expected
-	for(int i=0; i<DIM; i++){
-		for(int j=0; j<DIM; j++){
-			if((i==0 || i==DIM-1) && ((j+offset+1)%2)==0){
-				block_real_expected[i*DIM+j] = 1.;
-				block_imag_expected[i*DIM+j] = 0.;
-			}
-			else{
-				block_real_expected[i*DIM+j] = a;
-				block_imag_expected[i*DIM+j] = b;
-			}
-		}
-	}
+void CPUBlockTest::test_block_kernel_vertical() {
+    //Set Up
+    int DIM = 640;
+    int offset = 0;
+    float a = h_a, b = h_b;
 
-	//Process block_real, block_imag
-	block_kernel_vertical(offset, DIM, DIM, DIM, a , b, block_real, block_imag);
-	
-	Matrix matrix_processed(block_real, block_imag, DIM, DIM);
-	Matrix matrix_expected(block_real_expected, block_imag_expected, DIM, DIM);
-	
-	//Check
-	CPPUNIT_ASSERT( matrix_processed == matrix_expected );
-	std::cout << "TEST FUNCTION: block_kernel_vertical -> PASSED! " << std::endl;
+    float *block_real = new float[DIM * DIM];
+    float *block_imag = new float[DIM * DIM];
+    float *block_real_expected = new float[DIM * DIM];
+    float *block_imag_expected = new float[DIM * DIM];
+
+    //initialize block_real, block_imag
+    for(int i = 0; i < DIM; i++) {
+        for(int j = 0; j < DIM; j++) {
+            block_real[i * DIM + j] = 1.;
+            block_imag[i * DIM + j] = 0.;
+        }
+    }
+
+    //inizialize block_real_expected, block_imag_expected
+    for(int i = 0; i < DIM; i++) {
+        for(int j = 0; j < DIM; j++) {
+            if((i == 0 || i == DIM - 1) && ((j + offset + 1) % 2) == 0) {
+                block_real_expected[i * DIM + j] = 1.;
+                block_imag_expected[i * DIM + j] = 0.;
+            }
+            else {
+                block_real_expected[i * DIM + j] = a;
+                block_imag_expected[i * DIM + j] = b;
+            }
+        }
+    }
+
+    //Process block_real, block_imag
+    block_kernel_vertical(offset, DIM, DIM, DIM, a , b, block_real, block_imag);
+
+    Matrix matrix_processed(block_real, block_imag, DIM, DIM);
+    Matrix matrix_expected(block_real_expected, block_imag_expected, DIM, DIM);
+
+    //Check
+    CPPUNIT_ASSERT( matrix_processed == matrix_expected );
+    std::cout << "TEST FUNCTION: block_kernel_vertical -> PASSED! " << std::endl;
 }
 
-void CPUBlockTest::test_block_kernel_horizontal(){
-	//Set Up
-	int DIM=640;
-	int offset=0;
-	float a=h_a, b=h_b;
-	float *block_real = new float[DIM*DIM];
-    float *block_imag = new float[DIM*DIM];
-    float *block_real_expected = new float[DIM*DIM];
-    float *block_imag_expected = new float[DIM*DIM];
-    
-    //initialize block_real, block_imag
-    for(int i=0; i<DIM; i++){
-		for(int j=0; j<DIM; j++){
-			block_real[i*DIM+j] = 1.;
-			block_imag[i*DIM+j] = 0.;
-		}
-	}
-	
-	//inizialize block_real_expected, block_imag_expected
-	for(int i=0; i<DIM; i++){
-		for(int j=0; j<DIM; j++){
-			if((j==0 || j==DIM-1) && ((i+offset+1)%2)==0){
-				block_real_expected[i*DIM+j] = 1.;
-				block_imag_expected[i*DIM+j] = 0.;
-			}
-			else{
-				block_real_expected[i*DIM+j] = a;
-				block_imag_expected[i*DIM+j] = b;
-			}
-		}
-	}
+void CPUBlockTest::test_block_kernel_horizontal() {
+    //Set Up
+    int DIM = 640;
+    int offset = 0;
+    float a = h_a, b = h_b;
+    float *block_real = new float[DIM * DIM];
+    float *block_imag = new float[DIM * DIM];
+    float *block_real_expected = new float[DIM * DIM];
+    float *block_imag_expected = new float[DIM * DIM];
 
-	//Process block_real, block_imag
-	block_kernel_horizontal(offset, DIM, DIM, DIM, a , b, block_real, block_imag);
-	
-	Matrix matrix_processed(block_real, block_imag, DIM, DIM);
-	Matrix matrix_expected(block_real_expected, block_imag_expected, DIM, DIM);
-	
-	//Check
-	CPPUNIT_ASSERT( matrix_processed == matrix_expected );
-	std::cout << "TEST FUNCTION: block_kernel_horizontal -> PASSED! " << std::endl;
+    //initialize block_real, block_imag
+    for(int i = 0; i < DIM; i++) {
+        for(int j = 0; j < DIM; j++) {
+            block_real[i * DIM + j] = 1.;
+            block_imag[i * DIM + j] = 0.;
+        }
+    }
+
+    //inizialize block_real_expected, block_imag_expected
+    for(int i = 0; i < DIM; i++) {
+        for(int j = 0; j < DIM; j++) {
+            if((j == 0 || j == DIM - 1) && ((i + offset + 1) % 2) == 0) {
+                block_real_expected[i * DIM + j] = 1.;
+                block_imag_expected[i * DIM + j] = 0.;
+            }
+            else {
+                block_real_expected[i * DIM + j] = a;
+                block_imag_expected[i * DIM + j] = b;
+            }
+        }
+    }
+
+    //Process block_real, block_imag
+    block_kernel_horizontal(offset, DIM, DIM, DIM, a , b, block_real, block_imag);
+
+    Matrix matrix_processed(block_real, block_imag, DIM, DIM);
+    Matrix matrix_expected(block_real_expected, block_imag_expected, DIM, DIM);
+
+    //Check
+    CPPUNIT_ASSERT( matrix_processed == matrix_expected );
+    std::cout << "TEST FUNCTION: block_kernel_horizontal -> PASSED! " << std::endl;
 }
 
 //TEST cpublocksse
 
-void CPUBlockTest::test_update_shifty_sse(){
-	//Set Up
-	int DIM=4;
-	int offset=0;
-	float a=h_a, b=h_b;
-	float *block_r00 = new float[DIM*DIM];
-    float *block_i00 = new float[DIM*DIM];
-    float *block_r10 = new float[DIM*DIM];
-    float *block_i10 = new float[DIM*DIM];
-    float *block_r00_expected = new float[DIM*DIM];
-    float *block_i00_expected = new float[DIM*DIM];
-    float *block_r10_expected = new float[DIM*DIM];
-    float *block_i10_expected = new float[DIM*DIM];
-    
+void CPUBlockTest::test_update_shifty_sse() {
+    //Set Up
+    int DIM = 4;
+    int offset = 0;
+    float a = h_a, b = h_b;
+    float *block_r00 = new float[DIM * DIM];
+    float *block_i00 = new float[DIM * DIM];
+    float *block_r10 = new float[DIM * DIM];
+    float *block_i10 = new float[DIM * DIM];
+    float *block_r00_expected = new float[DIM * DIM];
+    float *block_i00_expected = new float[DIM * DIM];
+    float *block_r10_expected = new float[DIM * DIM];
+    float *block_i10_expected = new float[DIM * DIM];
+
     //initialize block_r00, block_i00, block_r10, block_i10
-    for(int i=0; i<DIM; i++){
-		for(int j=0; j<DIM; j++){
-			block_r00[i*DIM+j] = 1.;
-			block_i00[i*DIM+j] = 0.;
-			block_r10[i*DIM+j] = 1.;
-			block_i10[i*DIM+j] = 0.;
-		}
-	}
-	
-	//inizialize block_r00_expected, block_i00_expected
-	for(int i=0; i<DIM; i++){
-		for(int j=0; j<DIM; j++){
-			block_r00_expected[i*DIM+j] = a;
-			block_i00_expected[i*DIM+j] = b;
-			block_r10_expected[i*DIM+j] = a;
-			block_i10_expected[i*DIM+j] = b;
-		}
-	}
-	
-	//Process block_r00, block_i00, block_r10, block_i10
-	update_shifty_sse<0>(DIM, DIM, DIM, a, b, block_r00, block_i00, block_r10, block_i10);
-	
-	Matrix matrix_00_processed(block_r00, block_i00, DIM, DIM);
-	Matrix matrix_10_processed(block_r10, block_i10, DIM, DIM);
-	Matrix matrix_00_expected(block_r00_expected, block_i00_expected, DIM, DIM);
-	Matrix matrix_10_expected(block_r10_expected, block_i10_expected, DIM, DIM);
-	
-	//Check
-	CPPUNIT_ASSERT( matrix_00_processed == matrix_00_expected );
-	CPPUNIT_ASSERT( matrix_10_processed == matrix_10_expected );
-	std::cout << "TEST FUNCTION: update_shifty_sse -> PASSED! " << std::endl;
+    for(int i = 0; i < DIM; i++) {
+        for(int j = 0; j < DIM; j++) {
+            block_r00[i * DIM + j] = 1.;
+            block_i00[i * DIM + j] = 0.;
+            block_r10[i * DIM + j] = 1.;
+            block_i10[i * DIM + j] = 0.;
+        }
+    }
+
+    //inizialize block_r00_expected, block_i00_expected
+    for(int i = 0; i < DIM; i++) {
+        for(int j = 0; j < DIM; j++) {
+            block_r00_expected[i * DIM + j] = a;
+            block_i00_expected[i * DIM + j] = b;
+            block_r10_expected[i * DIM + j] = a;
+            block_i10_expected[i * DIM + j] = b;
+        }
+    }
+
+    //Process block_r00, block_i00, block_r10, block_i10
+    update_shifty_sse<0>(DIM, DIM, DIM, a, b, block_r00, block_i00, block_r10, block_i10);
+
+    Matrix matrix_00_processed(block_r00, block_i00, DIM, DIM);
+    Matrix matrix_10_processed(block_r10, block_i10, DIM, DIM);
+    Matrix matrix_00_expected(block_r00_expected, block_i00_expected, DIM, DIM);
+    Matrix matrix_10_expected(block_r10_expected, block_i10_expected, DIM, DIM);
+
+    //Check
+    CPPUNIT_ASSERT( matrix_00_processed == matrix_00_expected );
+    CPPUNIT_ASSERT( matrix_10_processed == matrix_10_expected );
+    std::cout << "TEST FUNCTION: update_shifty_sse -> PASSED! " << std::endl;
 }
 
-void CPUBlockTest::test_update_shiftx_sse(){
-	//Set Up
-	int DIM=4;
-	int offset=0;
-	float a=h_a, b=h_b;
-	float *block_r00 = new float[DIM*DIM];
-    float *block_i00 = new float[DIM*DIM];
-    float *block_r10 = new float[DIM*DIM];
-    float *block_i10 = new float[DIM*DIM];
-    float *block_r00_expected = new float[DIM*DIM];
-    float *block_i00_expected = new float[DIM*DIM];
-    float *block_r10_expected = new float[DIM*DIM];
-    float *block_i10_expected = new float[DIM*DIM];
-    
+void CPUBlockTest::test_update_shiftx_sse() {
+    //Set Up
+    int DIM = 4;
+    int offset = 0;
+    float a = h_a, b = h_b;
+    float *block_r00 = new float[DIM * DIM];
+    float *block_i00 = new float[DIM * DIM];
+    float *block_r10 = new float[DIM * DIM];
+    float *block_i10 = new float[DIM * DIM];
+    float *block_r00_expected = new float[DIM * DIM];
+    float *block_i00_expected = new float[DIM * DIM];
+    float *block_r10_expected = new float[DIM * DIM];
+    float *block_i10_expected = new float[DIM * DIM];
+
     //initialize block_r00, block_i00, block_r10, block_i10
-    for(int i=0; i<DIM; i++){
-		for(int j=0; j<DIM; j++){
-			block_r00[i*DIM+j] = 1.;
-			block_i00[i*DIM+j] = 0.;
-			block_r10[i*DIM+j] = 1.;
-			block_i10[i*DIM+j] = 0.;
-		}
-	}
-	
-	//inizialize block_r00_expected, block_i00_expected
-	for(int i=0; i<DIM; i++){
-		for(int j=0; j<DIM; j++){
-			block_r00_expected[i*DIM+j] = a;
-			block_i00_expected[i*DIM+j] = b;
-			block_r10_expected[i*DIM+j] = a;
-			block_i10_expected[i*DIM+j] = b;
-		}
-	}
-	
-	//Process block_r00, block_i00, block_r10, block_i10
-	update_shiftx_sse<0>(DIM, DIM, DIM, a, b, block_r00, block_i00, block_r10, block_i10);
-	
-	Matrix matrix_00_processed(block_r00, block_i00, DIM, DIM);
-	Matrix matrix_10_processed(block_r10, block_i10, DIM, DIM);
-	Matrix matrix_00_expected(block_r00_expected, block_i00_expected, DIM, DIM);
-	Matrix matrix_10_expected(block_r10_expected, block_i10_expected, DIM, DIM);
-	
-	//Check
-	CPPUNIT_ASSERT( matrix_00_processed == matrix_00_expected );
-	CPPUNIT_ASSERT( matrix_10_processed == matrix_10_expected );
-	std::cout << "TEST FUNCTION: update_shiftx_sse -> PASSED! " << std::endl;
+    for(int i = 0; i < DIM; i++) {
+        for(int j = 0; j < DIM; j++) {
+            block_r00[i * DIM + j] = 1.;
+            block_i00[i * DIM + j] = 0.;
+            block_r10[i * DIM + j] = 1.;
+            block_i10[i * DIM + j] = 0.;
+        }
+    }
+
+    //inizialize block_r00_expected, block_i00_expected
+    for(int i = 0; i < DIM; i++) {
+        for(int j = 0; j < DIM; j++) {
+            block_r00_expected[i * DIM + j] = a;
+            block_i00_expected[i * DIM + j] = b;
+            block_r10_expected[i * DIM + j] = a;
+            block_i10_expected[i * DIM + j] = b;
+        }
+    }
+
+    //Process block_r00, block_i00, block_r10, block_i10
+    update_shiftx_sse<0>(DIM, DIM, DIM, a, b, block_r00, block_i00, block_r10, block_i10);
+
+    Matrix matrix_00_processed(block_r00, block_i00, DIM, DIM);
+    Matrix matrix_10_processed(block_r10, block_i10, DIM, DIM);
+    Matrix matrix_00_expected(block_r00_expected, block_i00_expected, DIM, DIM);
+    Matrix matrix_10_expected(block_r10_expected, block_i10_expected, DIM, DIM);
+
+    //Check
+    CPPUNIT_ASSERT( matrix_00_processed == matrix_00_expected );
+    CPPUNIT_ASSERT( matrix_10_processed == matrix_10_expected );
+    std::cout << "TEST FUNCTION: update_shiftx_sse -> PASSED! " << std::endl;
 }
 
 
 //Members of class Matrix
-Matrix::Matrix(float *matrix_real, float *matrix_imag, int width, int height){
-	m_real = new float[width*height];
-	m_imag = new float[width*height];
-	m_width = width;
-	m_height = height;
-	
-	for(int i=0; i<m_height; i++){
-		for(int j=0; j<m_width; j++){
-			m_real[i*m_width+j] = matrix_real[i*m_width+j];
-			m_imag[i*m_width+j] = matrix_imag[i*m_width+j];
-		}
-	}
+Matrix::Matrix(float *matrix_real, float *matrix_imag, int width, int height) {
+    m_real = new float[width * height];
+    m_imag = new float[width * height];
+    m_width = width;
+    m_height = height;
+
+    for(int i = 0; i < m_height; i++) {
+        for(int j = 0; j < m_width; j++) {
+            m_real[i * m_width + j] = matrix_real[i * m_width + j];
+            m_imag[i * m_width + j] = matrix_imag[i * m_width + j];
+        }
+    }
 }
 
-void Matrix::show_matrix(){
-	for(int i=0; i<m_height; i++){
-		for(int j=0; j<m_width; j++){
-			std::cout << "(" << m_real[i*m_height+j] << " , " << m_imag[i*m_height+j] << ") ";
-		}
-		std::cout << std::endl;
-	}
+void Matrix::show_matrix() {
+    for(int i = 0; i < m_height; i++) {
+        for(int j = 0; j < m_width; j++) {
+            std::cout << "(" << m_real[i * m_height + j] << " , " << m_imag[i * m_height + j] << ") ";
+        }
+        std::cout << std::endl;
+    }
 }
 
 bool Matrix::operator ==(const Matrix &other) const {
-	bool var=false;
-	if((m_height == other.m_height) && (m_width == other.m_width)){
-		var = true;
-		for(int i=0; i<m_height; i++){
-			for(int j=0; j<m_width; j++){
-				if((m_real[i*m_width+j] != other.m_real[i*m_width+j]) || (m_imag[i*m_width+j] != other.m_imag[i*m_width+j]))
-					var = false;
-			}
-		}
-	}
-	return var;
+    bool var = false;
+    if((m_height == other.m_height) && (m_width == other.m_width)) {
+        var = true;
+        for(int i = 0; i < m_height; i++) {
+            for(int j = 0; j < m_width; j++) {
+                if((m_real[i * m_width + j] != other.m_real[i * m_width + j]) || (m_imag[i * m_width + j] != other.m_imag[i * m_width + j]))
+                    var = false;
+            }
+        }
+    }
+    return var;
 }
-	
+
