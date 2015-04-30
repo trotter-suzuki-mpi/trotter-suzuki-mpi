0f08bbdd05a36c170e748f15ac093f8d0a91e971
Luca Calderaro <calderaroluca@gmail.com>
2015-04-30 18:57:05 +0200

    Update unittest.cpp

--- test/unittest.cpp
+++ test/unittest.cpp
@@ -21,8 +21,163 @@
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/ui/text/TestRunner.h>
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string>
+#include <sstream>
+
+#include <fstream>
+#include <unistd.h>
+#include <stdlib.h>
+#include <iostream>
+#include <complex>
+
+#include "trotter.h"
+#include "common.h"
+
+#define DIM 640
+#define ITERATIONS 1000
+#define KERNEL_TYPE 0
+#define SNAPSHOTS 0
+
+//external potential operator in coordinate representation
+void potential_op_coord_representation(float *hamilt_pot, int dimx, int dimy) {
+    float constant = 0.;
+    for(int i = 0; i < dimy; i++) {
+        for(int j = 0; j < dimx; j++) {
+            hamilt_pot[i * dimx + j] = constant;
+        }
+    }
+}
+
+void init_state(float *p_real, float *p_imag, int dimx, int dimy) {
+    double s = 64.0; // FIXME: y esto?
+    double L_x = dimx;
+    double L_y = dimy;
+    double n_x = 1., n_y = 1.;
+
+    for (int y = 1; y <= dimy; y++) {
+        for (int x = 1; x <= dimx; x++) {
+            //std::complex<float> tmp = std::complex<float>(exp(-(pow(x - 180.0, 2.0) + pow(y - 300.0, 2.0)) / (2.0 * pow(s, 2.0))), 0.0)
+            //                        * exp(std::complex<float>(0.0, 0.4 * (x + y - 480.0)));
+
+            std::complex<float> tmp = std::complex<float> (sin(2 * 3.14159 / L_x * x) * sin(2 * 3.14159 / L_y * y), 0.0);
+
+            p_real[y * dimx + x] = real(tmp);
+            p_imag[y * dimx + x] = imag(tmp);
+        }
+    }
+}
+
+//calculate potential part of evolution operator
+void init_pot_evolution_op(float * hamilt_pot, float * external_pot_real, float * external_pot_imag, int dimx, int dimy, double particle_mass, double time_single_it ) {
+    float CONST_1 = -1. * time_single_it;
+    float CONST_2 = 2. * time_single_it / particle_mass;		//CONST_2: discretization of momentum operator and the only effect is to produce a scalar operator, so it could be omitted
+
+    std::complex<float> tmp;
+    for(int i = 0; i < dimy; i++) {
+        for(int j = 0; j < dimx; j++) {
+            tmp = exp(std::complex<float> (0., CONST_1 * hamilt_pot[i * dimx + j] + CONST_2));
+            external_pot_real[i * dimx + j] = real(tmp);
+            external_pot_imag[i * dimx + j] = imag(tmp);
+        }
+    }
+}
+
+void print_usage() {
+    std::cout << "\nTest some functions of CPU and CPU/SSE kernels and simulate\n"\
+			  "the evolution of a particle in a box.\n\n"\
+			  "Usage:\n" \
+              "     unittest [OPTION]\n" \
+              "Arguments:\n" \
+              "     -d NUMBER     Matrix dimension (default: " << DIM << ")\n" \
+              "     -i NUMBER     Number of iterations (default: " << ITERATIONS << ")\n" \
+              "     -k NUMBER     Kernel type (default: " << KERNEL_TYPE << "): \n" \
+              "                      0: CPU, cache-optimized\n" \
+              "                      1: CPU, SSE and cache-optimized\n" \
+              "                      2: GPU\n" \
+              "                      3: Hybrid (experimental) \n" \
+              "     -s NUMBER     Snapshots are taken at every NUMBER of iterations.\n" \
+              "                   Zero means no snapshots. Default: " << SNAPSHOTS << ".\n"\
+              "     -v            Calculates expected values of energy and momentum operators\n"\
+              "                   once the simulation is finished";
+}
+
+void process_command_line(int argc, char** argv, int *dim, int *iterations, int *snapshots, int *kernel_type, bool *values) {
+    // Setting default values
+    *dim = DIM;
+    *iterations = ITERATIONS;
+    *snapshots = SNAPSHOTS;
+    *kernel_type = KERNEL_TYPE;
+
+    int c;
+    while ((c = getopt (argc, argv, "d:hi:k:s:v")) != -1) {
+        switch (c) {
+        case 'd':
+            *dim = atoi(optarg);
+            if (*dim <= 0) {
+                fprintf (stderr, "The argument of option -d should be a positive integer.\n");
+                abort ();
+            }
+            break;
+        case 'i':
+            *iterations = atoi(optarg);
+            if (*iterations <= 0) {
+                fprintf (stderr, "The argument of option -i should be a positive integer.\n");
+                abort ();
+            }
+            break;
+        case 'h':
+            print_usage();
+            abort ();
+            break;
+        case 'k':
+            *kernel_type = atoi(optarg);
+            if (*kernel_type < 0 || *kernel_type > 3) {
+                fprintf (stderr, "The argument of option -k should be a valid kernel.\n");
+                abort ();
+            }
+            break;
+        case 's':
+            *snapshots = atoi(optarg);
+            if (*snapshots <= 0) {
+                fprintf (stderr, "The argument of option -s should be a positive integer.\n");
+                abort ();
+            }
+            break;
+        case 'v':
+            *values = true;
+            break;
+        case '?':
+            if (optopt == 'd' || optopt == 'i' || optopt == 'k' || optopt == 's') {
+                fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+                print_usage();
+                abort ();
+            }
+            else if (isprint (optopt)) {
+                fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+                print_usage();
+                abort ();
+            }
+            else {
+                fprintf (stderr, "Unknown option character `\\x%x'.\n",  optopt);
+                print_usage();
+                abort ();
+            }
+        default:
+            abort ();
+        }
+    }
+}
+
 int main(int argc, char** argv) {
 
+	int dim = 0, iterations = 0, snapshots = 0, kernel_type = 0;
+    int periods[2] = {0,0};
+    bool values = false;
+
+    process_command_line(argc, argv, &dim, &iterations, &snapshots, &kernel_type, &values);
+    
     // Get the top level suite from the registry
     CppUnit::Test *suite = CppUnit::TestFactoryRegistry::getRegistry().makeTest();
     // Adds the test to the list of test to run
@@ -33,5 +188,51 @@ int main(int argc, char** argv) {
     // Run the tests.
     bool wasSucessful = runner.run();
     // Return error code 1 if the one of test failed.
-    return wasSucessful ? 0 : 1;
+    //return wasSucessful ? 0 : 1;
+    
+    //set hamiltonian variables
+    const double particle_mass = 1.;
+    float *hamilt_pot = new float[dim * dim];
+    potential_op_coord_representation(hamilt_pot, dim, dim);	//set potential operator
+
+    //set and calculate evolution operator variables from hamiltonian
+    const double time_single_it = 0.08 * particle_mass / 2.;	//second approx trotter-suzuki: time/2
+    float *external_pot_real = new float[dim * dim];
+    float *external_pot_imag = new float[dim * dim];
+    init_pot_evolution_op(hamilt_pot, external_pot_real, external_pot_imag, dim, dim, particle_mass, time_single_it);	//calculate potential part of evolution operator
+    static const double h_a = cos(time_single_it / (2. * particle_mass));
+    static const double h_b = sin(time_single_it / (2. * particle_mass));
+
+    //set initial state
+    float *p_real = new float[dim * dim];
+    float *p_imag = new float[dim * dim];
+    init_state(p_real, p_imag, dim, dim);
+
+    //set file output directory
+    std::stringstream filename;
+    std::string filenames;
+    if(snapshots) {
+        int status;
+
+        filename.str("");
+        filename << "D" << dim << "_I" << iterations << "_S" << snapshots << "";
+        filenames = filename.str();
+
+        status = mkdir(filenames.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+
+        if(status != 0 && status != -1)
+            filenames = "./";
+    }
+    else
+        filenames = "./";
+    
+	std::cout << "Simulation started\n";
+    trotter(h_a, h_b, external_pot_real, external_pot_imag, p_real, p_imag, dim, dim, iterations, snapshots, kernel_type, periods, argc, argv, filenames.c_str());
+	
+	
+    if(values)
+		std::cout << "Calculating expected values\n";
+        expect_values(dim, iterations, snapshots, hamilt_pot, particle_mass, filenames.c_str());
+
+    return 0;
 }
