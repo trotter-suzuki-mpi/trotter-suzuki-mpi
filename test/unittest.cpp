8b782c0d63c68e21b7e7d1f3ff753ddc421202c3
Luca Calderaro <calderaroluca@gmail.com>
2015-05-11 10:42:56 +0200

    Update unittest.cpp

--- test/unittest.cpp
+++ test/unittest.cpp
@@ -31,6 +31,7 @@
 #include <stdlib.h>
 #include <iostream>
 #include <complex>
+#include "mpi.h"
 
 #include "trotter.h"
 #include "common.h"
@@ -52,23 +53,81 @@ void potential_op_coord_representation(float *hamilt_pot, int dimx, int dimy, in
 
 void init_state(float *p_real, float *p_imag, int dimx, int dimy, int halo_x, int halo_y, int *periods) {
     double s = 64.0; // FIXME: y esto?
-    double L_x = dimx - periods[1]*2*halo_x;
-    double L_y = dimy - periods[0]*2*halo_y;
+    double L_x = dimx - periods[1] * 2 * halo_x;
+    double L_y = dimy - periods[0] * 2 * halo_y;
     double n_x = 1., n_y = 1.;
 
     for (int y = 1; y <= dimy; y++) {
         for (int x = 1; x <= dimx; x++) {
             //std::complex<float> tmp = std::complex<float>(exp(-(pow(x - 180.0, 2.0) + pow(y - 300.0, 2.0)) / (2.0 * pow(s, 2.0))), 0.0)
-              //                      * exp(std::complex<float>(0.0, 0.4 * (x + y - 480.0)));
+            //                      * exp(std::complex<float>(0.0, 0.4 * (x + y - 480.0)));
 
-            std::complex<float> tmp = std::complex<float> (sin(2 * 3.14159 / L_x * (x - periods[1]*halo_x)) * sin(2 * 3.14159 / L_y * (y - periods[0]*halo_y)), 0.0);
-			
-			//std::complex<float> tmp = exp(std::complex<float>(0. , 2 * 3.14159 / L_x * (x - periods[1]*halo_x) + 2 * 3.14159 / L_y * (y - periods[0]*halo_y) ));
-			
+            std::complex<float> tmp = std::complex<float> (sin(2 * 3.14159 / L_x * (x - periods[1] * halo_x)) * sin(2 * 3.14159 / L_y * (y - periods[0] * halo_y)), 0.0);
+
+            //std::complex<float> tmp = exp(std::complex<float>(0. , 2 * 3.14159 / L_x * (x - periods[1]*halo_x) + 2 * 3.14159 / L_y * (y - periods[0]*halo_y) ));
+
             p_real[y * dimx + x] = real(tmp);
             p_imag[y * dimx + x] = imag(tmp);
         }
     }
+}
+
+void read_initial_state(float *p_real, float *p_imag, int dimx, int dimy, char *file_name, int halo_x, int halo_y, int *periods) {
+    std::ifstream input(file_name);
+
+    int in_width = dimx - 2 * periods[1] * halo_x;
+    int in_height = dimy - 2 * periods[0] * halo_y;
+    std::complex<float> tmp;
+    for(int i = 0, idy = periods[0] * halo_y ; i < in_height; i++, idy++) {
+        for(int j = 0, idx = periods[1] * halo_x ; j < in_width; j++, idx++) {
+            input >> tmp;
+            p_real[idy * dimx + idx] = real(tmp);
+            p_imag[idy * dimx + idx] = imag(tmp);
+
+            //Down band
+            if(i < halo_y && periods[0] != 0) {
+                p_real[(idy + in_height) * dimx + idx] = real(tmp);
+                p_imag[(idy + in_height) * dimx + idx] = imag(tmp);
+                //Down right corner
+                if(j < halo_x && periods[1] != 0) {
+                    p_real[(idy + in_height) * dimx + idx + in_width] = real(tmp);
+                    p_imag[(idy + in_height) * dimx + idx + in_width] = imag(tmp);
+                }
+                //Down left corner
+                if(j >= in_width - halo_x && periods[1] != 0) {
+                    p_real[(idy + in_height) * dimx + idx - in_width] = real(tmp);
+                    p_imag[(idy + in_height) * dimx + idx - in_width] = imag(tmp);
+                }
+            }
+
+            //Upper band
+            if(i >= in_height - halo_y && periods[0] != 0) {
+                p_real[(idy - in_height) * dimx + idx] = real(tmp);
+                p_imag[(idy - in_height) * dimx + idx] = imag(tmp);
+                //Up right corner
+                if(j < halo_x && periods[1] != 0) {
+                    p_real[(idy - in_height) * dimx + idx + in_width] = real(tmp);
+                    p_imag[(idy - in_height) * dimx + idx + in_width] = imag(tmp);
+                }
+                //Up left corner
+                if(j >= in_width - halo_x && periods[1] != 0) {
+                    p_real[(idy - in_height) * dimx + idx - in_width] = real(tmp);
+                    p_imag[(idy - in_height) * dimx + idx - in_width] = imag(tmp);
+                }
+            }
+            //Right band
+            if(j < halo_x && periods[1] != 0) {
+                p_real[idy * dimx + idx + in_width] = real(tmp);
+                p_imag[idy * dimx + idx + in_width] = imag(tmp);
+            }
+            //Left band
+            if(j >= in_width - halo_x && periods[1] != 0) {
+                p_real[idy * dimx + idx - in_width] = real(tmp);
+                p_imag[idy * dimx + idx - in_width] = imag(tmp);
+            }
+        }
+    }
+    input.close();
 }
 
 //calculate potential part of evolution operator
@@ -88,8 +147,8 @@ void init_pot_evolution_op(float * hamilt_pot, float * external_pot_real, float 
 
 void print_usage() {
     std::cout << "\nTest some functions of CPU and CPU/SSE kernels and simulate\n"\
-			  "the evolution of a particle in a box.\n\n"\
-			  "Usage:\n" \
+              "the evolution of a particle in a box.\n\n"\
+              "Usage:\n" \
               "     unittest [OPTION]\n" \
               "Arguments:\n" \
               "     -d NUMBER     Matrix dimension (default: " << DIM << ")\n" \
@@ -101,11 +160,12 @@ void print_usage() {
               "                      3: Hybrid (experimental) \n" \
               "     -s NUMBER     Snapshots are taken at every NUMBER of iterations.\n" \
               "                   Zero means no snapshots. Default: " << SNAPSHOTS << ".\n"\
+              "     -n STRING     Set initial state from a file.\n"\
               "     -v            Calculates expected values of energy and momentum operators\n"\
-              "                   once the simulation is finished";
+              "                   once the simulation is finished.";
 }
 
-void process_command_line(int argc, char** argv, int *dim, int *iterations, int *snapshots, int *kernel_type, bool *values) {
+void process_command_line(int argc, char** argv, int *dim, int *iterations, int *snapshots, int *kernel_type, bool *values, char *file_name) {
     // Setting default values
     *dim = DIM;
     *iterations = ITERATIONS;
@@ -113,7 +173,7 @@ void process_command_line(int argc, char** argv, int *dim, int *iterations, int 
     *kernel_type = KERNEL_TYPE;
 
     int c;
-    while ((c = getopt (argc, argv, "d:hi:k:s:v")) != -1) {
+    while ((c = getopt (argc, argv, "d:hi:k:s:vn:")) != -1) {
         switch (c) {
         case 'd':
             *dim = atoi(optarg);
@@ -150,6 +210,10 @@ void process_command_line(int argc, char** argv, int *dim, int *iterations, int 
         case 'v':
             *values = true;
             break;
+        case 'n':
+            for(int i = 0; i < 100; i++)
+                file_name[i] = optarg[i];
+            break;
         case '?':
             if (optopt == 'd' || optopt == 'i' || optopt == 'k' || optopt == 's') {
                 fprintf (stderr, "Option -%c requires an argument.\n", optopt);
@@ -173,12 +237,14 @@ void process_command_line(int argc, char** argv, int *dim, int *iterations, int 
 }
 
 int main(int argc, char** argv) {
-	int dim = 0, iterations = 0, snapshots = 0, kernel_type = 0;
-    int periods[2] = {1,1};
+    int dim = 0, iterations = 0, snapshots = 0, kernel_type = 0;
+    int periods[2] = {1, 1};
+    char file_name[100];
+    file_name[0] = '\0';
     bool values = false;
 
-    process_command_line(argc, argv, &dim, &iterations, &snapshots, &kernel_type, &values);
-    
+    process_command_line(argc, argv, &dim, &iterations, &snapshots, &kernel_type, &values, file_name);
+
     // Get the top level suite from the registry
     CppUnit::Test *suite = CppUnit::TestFactoryRegistry::getRegistry().makeTest();
     // Adds the test to the list of test to run
@@ -190,13 +256,13 @@ int main(int argc, char** argv) {
     bool wasSucessful = runner.run();
     // Return error code 1 if the one of test failed.
     if(!wasSucessful)
-	return 1;
-    
+        return 1;
+
     int halo_x = (kernel_type == 2 ? 3 : 4);
     int halo_y = 4;
-	int matrix_width = dim + periods[1]*2*halo_x;
-	int matrix_height = dim + periods[0]*2*halo_y;
-	
+    int matrix_width = dim + periods[1] * 2 * halo_x;
+    int matrix_height = dim + periods[0] * 2 * halo_y;
+
     //set hamiltonian variables
     const double particle_mass = 1.;
     float *hamilt_pot = new float[matrix_width * matrix_height];
@@ -213,13 +279,16 @@ int main(int argc, char** argv) {
     //set initial state
     float *p_real = new float[matrix_width * matrix_height];
     float *p_imag = new float[matrix_width * matrix_height];
-    init_state(p_real, p_imag, matrix_width, matrix_height, halo_x, halo_y, periods);
+    if(file_name[0] == '\0')
+        init_state(p_real, p_imag, matrix_width, matrix_height, halo_x, halo_y, periods);
+    else
+        read_initial_state(p_real, p_imag, matrix_width, matrix_height, file_name, halo_x, halo_y, periods);
 
     //set file output directory
     std::stringstream filename;
     std::string filenames;
     if(snapshots) {
-        int status;
+        int status = 0;
 
         filename.str("");
         filename << "D" << dim << "_I" << iterations << "_S" << snapshots << "";
@@ -232,14 +301,14 @@ int main(int argc, char** argv) {
     }
     else
         filenames = "./";
-    
-	std::cout << "Simulation started\n";
-    trotter(h_a, h_b, external_pot_real, external_pot_imag, p_real, p_imag, matrix_width, matrix_height, iterations, snapshots, kernel_type, periods, argc, argv, filenames.c_str());
-	
-	
-    if(values)
-		std::cout << "Calculating expected values\n";
-        expect_values(dim, iterations, snapshots, hamilt_pot, particle_mass, filenames.c_str());
+
+    std::cout << "Simulation started\n";
+    procs_topology var = trotter(h_a, h_b, external_pot_real, external_pot_imag, p_real, p_imag, matrix_width, matrix_height, iterations, snapshots, kernel_type, periods, argc, argv, filenames.c_str());
+
+    if((values == true) && (var.rank == 0)) {
+        std::cout << "Calculating expected values\n";
+        expect_values(dim, iterations, snapshots, hamilt_pot, particle_mass, filenames.c_str(), var, periods, halo_x, halo_y);
+    }
 
     return 0;
 }
