fba19d74b7700cbe1b18aba1db1021e54c0af9d6
Luca Calderaro <calderaroluca@gmail.com>
2015-05-15 17:55:38 +0200

    Update single2double.sh

--- single2double.sh
+++ single2double.sh
@@ -12,3 +12,27 @@ done
 cat cpublocksse.cpp|sed 's/% 4; j += 4, idx1 += 4, idx2 += 4/% 2; j += 2, idx1 += 2, idx2 += 2/' > cpublocksse.cpp.new
 mv cpublocksse.cpp.new cpublocksse.cpp
 cd ..
+
+cd test
+for file in `ls *.cpp *.h *.cu`; do
+  cat $file|sed 's/float/double/g'|sed 's/ssyev/dsyev/g'|sed 's/slamch/dlamch/g'|\
+            sed 's/sgemm/dgemm/g'|sed 's/ssymm/dsymm/g'|\
+            sed 's/Sgemm/Dgemm/g'|sed 's/Ssymm/Dsymm/g'|\
+            sed 's/_ps/_pd/g' | sed 's/__m128/__m128d/g'|\
+            sed 's/__f/__d/g' |\
+            sed 's/MPI_FLOAT/MPI_DOUBLE/g' > $file.new
+  mv $file.new $file
+done
+cd ..
+
+cd examples
+for file in `ls *.cpp *.h *.cu`; do
+  cat $file|sed 's/float/double/g'|sed 's/ssyev/dsyev/g'|sed 's/slamch/dlamch/g'|\
+            sed 's/sgemm/dgemm/g'|sed 's/ssymm/dsymm/g'|\
+            sed 's/Sgemm/Dgemm/g'|sed 's/Ssymm/Dsymm/g'|\
+            sed 's/_ps/_pd/g' | sed 's/__m128/__m128d/g'|\
+            sed 's/__f/__d/g' |\
+            sed 's/MPI_FLOAT/MPI_DOUBLE/g' > $file.new
+  mv $file.new $file
+done
+cd ..
