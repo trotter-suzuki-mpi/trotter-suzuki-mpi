60952c84a7f9fc4c9d070e628da97ff0e86ec620
Lucacalderaro <calderaroluca@gmail.com>
2015-05-25 15:40:36 +0200

    two particles example added

---
+++ examples/two_particles_exponential_initial_state.cpp
@@ -0,0 +1,156 @@
+/**
+ * Distributed Trotter-Suzuki solver
+ * Copyright (C) 2012 Peter Wittek, 2010-2012 Carlos Bederián, 2015 Luca Calderaro
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/**
+ * This source provides an example of the trotter-suzuki program.
+ * It calculates the time-evolution of two free particles in a box, where the initial
+ * state for each particle is the following:
+ * 		exp(i2M_PI / L (x + y))
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string>
+#include <sstream>
+
+#include <fstream>
+#include <unistd.h>
+#include <stdlib.h>
+#include <iostream>
+#include <complex>
+#include "mpi.h"
+#include "common.h"
+#include "trotter.h"
+
+#define DIM 640
+#define ITERATIONS 1000
+#define KERNEL_TYPE 0
+#define SNAPSHOTS 100
+#define PARTICLES_NUMBER 2
+
+//set external potential operator in coordinate representation
+void potential_op_coord_representation(double *hamilt_pot, int dimx, int dimy, int halo_x, int halo_y, int *periods) {
+    double constant = 0.;
+    for(int i = 0; i < dimy; i++) {
+        for(int j = 0; j < dimx; j++) {
+            hamilt_pot[i * dimx + j] = constant;
+        }
+    }
+}
+
+//set initial state
+void init_state(double *p_real, double *p_imag, int dimx, int dimy, int halo_x, int halo_y, int *periods, int Particles_number) {
+    double s = 64.0; // FIXME: y esto?
+    double L_x = dimx - periods[1] * 2 * halo_x;
+    double L_y = dimy - periods[0] * 2 * halo_y;
+    double n_x = 1., n_y = 1.;
+
+    int offset = 0;
+    for (int i = 0; i < Particles_number; i++) {
+		for (int y = 1; y <= dimy; y++) {
+			for (int x = 1; x <= dimx; x++) {
+				std::complex<double> tmp = exp(std::complex<double>(0. , 2 * 3.14159 / L_x * (x - periods[1] * halo_x) + 2 * 3.14159 / L_y * (y - periods[0] * halo_y) ));
+
+				p_real[y * dimx + x + offset] = real(tmp);
+				p_imag[y * dimx + x + offset] = imag(tmp);
+			}
+		}
+		offset += dimx * dimy;
+	}
+}
+
+//calculate potential part of evolution operator
+void init_pot_evolution_op(double * hamilt_pot, double * external_pot_real, double * external_pot_imag, int dimx, int dimy, double particle_mass, double time_single_it ) {
+    double CONST_1 = -1. * time_single_it;
+    double CONST_2 = 2. * time_single_it / particle_mass;		//CONST_2: discretization of momentum operator and the only effect is to produce a scalar operator, so it could be omitted
+
+    std::complex<double> tmp;
+    for(int i = 0; i < dimy; i++) {
+        for(int j = 0; j < dimx; j++) {
+            tmp = exp(std::complex<double> (0., CONST_1 * hamilt_pot[i * dimx + j] + CONST_2));
+            external_pot_real[i * dimx + j] = real(tmp);
+            external_pot_imag[i * dimx + j] = imag(tmp);
+        }
+    }
+}
+
+int main(int argc, char** argv) {
+    int dim = DIM, iterations = ITERATIONS, snapshots = SNAPSHOTS, kernel_type = KERNEL_TYPE, Particles_number = PARTICLES_NUMBER;
+    int periods[2] = {1, 1};
+    bool show_time_sim = true;
+    bool imag_time = false;
+    int halo_x = (kernel_type == 2 ? 3 : 4);
+    int halo_y = 4;
+    int matrix_width = dim + periods[1] * 2 * halo_x;
+    int matrix_height = dim + periods[0] * 2 * halo_y;
+
+    //set hamiltonian variables
+    const double particle_mass = 1.;
+    double *hamilt_pot = new double[matrix_width * matrix_height];
+
+    potential_op_coord_representation(hamilt_pot, matrix_width, matrix_height, halo_x, halo_y, periods);	//set potential operator
+
+    //set and calculate evolution operator variables from hamiltonian
+    const double time_single_it = 0.08 * particle_mass / 2.;	//second approx trotter-suzuki: time/2
+    double *external_pot_real = new double[matrix_width * matrix_height];
+    double *external_pot_imag = new double[matrix_width * matrix_height];
+    init_pot_evolution_op(hamilt_pot, external_pot_real, external_pot_imag, matrix_width, matrix_height, particle_mass, time_single_it);	//calculate potential part of evolution operator
+    double h_a = cos(time_single_it / (2. * particle_mass));
+    double h_b = sin(time_single_it / (2. * particle_mass));
+
+    //set initial state
+    double *p_real = new double[Particles_number * matrix_width * matrix_height];
+    double *p_imag = new double[Particles_number * matrix_width * matrix_height];
+    init_state(p_real, p_imag, matrix_width, matrix_height, halo_x, halo_y, periods, Particles_number);
+
+    MPI_Init(&argc, &argv);
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if(rank == 0) {
+        std::cout << "\n* This source provides an example of the trotter-suzuki program.\n";
+        std::cout << "* It calculates the time-evolution of two free particles in a box\n";
+        std::cout << "* with periodic boundary conditions, where the initial\n";
+        std::cout << "* state for each particle is the following:\n";
+        std::cout << "* \texp(i2M_PI / L (x + y))\n\n";
+    }
+
+    //set file output directory
+    std::stringstream dirname;
+    std::string dirnames;
+    if(snapshots) {
+        int status;
+
+        dirname.str("");
+        dirname << "D" << dim << "_I" << iterations << "_S" << snapshots << "";
+        dirnames = dirname.str();
+
+        status = mkdir(dirnames.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+
+        if(status != 0 && status != -1)
+            dirnames = ".";
+    }
+    else
+        dirnames = ".";
+
+    for(int i = 0; i < Particles_number; i++)
+        trotter(h_a, h_b, external_pot_real, external_pot_imag, &p_real[i * matrix_width * matrix_height], &p_imag[i * matrix_width * matrix_height], matrix_width, matrix_height, iterations, snapshots, kernel_type, periods, argc, argv, dirnames.c_str(), show_time_sim, imag_time, i + 1);
+    
+    MPI_Finalize();
+    return 0;
+}
