9b2115b547b50006acbcd9c45ec639c07d368300
Luca Calderaro <calderaroluca@gmail.com>
2015-09-29 17:17:03 +0200

    Normalization + nonlinear Schrodinger

--- src/cpublock.h
+++ src/cpublock.h
@@ -17,6 +17,8 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
+ 
+ //added delta_x, delta_y, coupling_const
 
 #ifndef __CPUBLOCK_H
 #define __CPUBLOCK_H
@@ -39,12 +41,12 @@ void block_kernel_horizontal(size_t start_offset, size_t stride, size_t width, s
 void block_kernel_vertical_imaginary(size_t start_offset, size_t stride, size_t width, size_t height, double a, double b, double * p_real, double * p_imag);
 void block_kernel_horizontal_imaginary(size_t start_offset, size_t stride, size_t width, size_t height, double a, double b, double * p_real, double * p_imag);
 
-void process_sides(size_t tile_width, size_t block_width, size_t halo_x, size_t read_y, size_t read_height, size_t write_offset, size_t write_height, double a, double b, const double *external_pot_real, const double *external_pot_imag, const double * p_real, const double * p_imag, double * next_real, double * next_imag, double * block_real, double * block_imag, bool imag_time);
-void process_band(size_t tile_width, size_t block_width, size_t block_height, size_t halo_x, size_t read_y, size_t read_height, size_t write_offset, size_t write_height, double a, double b, const double *external_pot_real, const double *external_pot_imag, const double * p_real, const double * p_imag, double * next_real, double * next_imag, int inner, int sides, bool imag_time);
+void process_sides(size_t tile_width, size_t block_width, size_t halo_x, size_t read_y, size_t read_height, size_t write_offset, size_t write_height, double a, double b, double coupling_const, const double *external_pot_real, const double *external_pot_imag, const double * p_real, const double * p_imag, double * next_real, double * next_imag, double * block_real, double * block_imag, bool imag_time);
+void process_band(size_t tile_width, size_t block_width, size_t block_height, size_t halo_x, size_t read_y, size_t read_height, size_t write_offset, size_t write_height, double a, double b, double coupling_const, const double *external_pot_real, const double *external_pot_imag, const double * p_real, const double * p_imag, double * next_real, double * next_imag, int inner, int sides, bool imag_time);
 
 class CPUBlock: public ITrotterKernel {
 public:
-    CPUBlock(double *p_real, double *p_imag, double *_external_pot_real, double *_external_pot_imag, double a, double b, int matrix_width, int matrix_height, int halo_x, int halo_y, int *_periods, bool imag_time
+    CPUBlock(double *p_real, double *p_imag, double *_external_pot_real, double *_external_pot_imag, double a, double b, double _coupling_const, double _delta_x, double _delta_y, int matrix_width, int matrix_height, int halo_x, int halo_y, int *_periods, double _norm, bool imag_time
 #ifdef HAVE_MPI
              , MPI_Comm cartcomm
 #endif
@@ -75,13 +77,17 @@ private:
     double *external_pot_imag;
     double a;
     double b;
+    double delta_x, delta_y;
+    double norm;
+    double coupling_const;
     int sense;
     size_t halo_x, halo_y, tile_width, tile_height;
     bool imag_time;
     static const size_t block_width = BLOCK_WIDTH;
     static const size_t block_height = BLOCK_HEIGHT;
 
-    int start_x, inner_end_x, start_y, inner_start_y,  inner_end_y;
+    int start_x, inner_end_x, start_y, inner_start_y, inner_end_y;
+    int inner_start_x, end_x, end_y;
     int *periods;
 #ifdef HAVE_MPI
     MPI_Comm cartcomm;
