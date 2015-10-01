d257bee8a9f98cafdfab786ef386af738d95492a
Peter Wittek <peterwittek@users.noreply.github.com>
2015-10-01 11:56:58 +0200

    Python and MATLAB shell scripts moved to Makefile targets

--- src/python/makepy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-rm -rf *.h *.i *.cpp *.cu *.cxx dist trottersuzuki.egg* build trottersuzuki.py *.so
-cp ../*.h .
-cp ../*.i .
-cp ../*.cpp .
-cp ../*.cu .
-swig -c++ -python trotter.i
-python2 setup.py build_ext --inplace
