faa0359bac556ce5e7f76178d852a1fb06b6a893
Lucacalderaro <calderaroluca@gmail.com>
2015-11-30 11:57:26 +0100

    Single functions to calculate mean position and momentum

--- src/common.h
+++ src/common.h
@@ -85,6 +85,10 @@ double Energy_tot(double * p_real, double * p_imag,
 				  double particle_mass, double coupling_const, double (*hamilt_pot)(int x, int y, int matrix_width, int matrix_height, int * periods, int halo_x, int halo_y), double * external_pot, double omega, double coord_rot_x, double coord_rot_y,
 				  double delta_x, double delta_y, double norm2, int inner_start_x, int start_x, int inner_end_x, int end_x, int inner_start_y, int start_y, int inner_end_y, int end_y,
 				  int matrix_width, int matrix_height, int halo_x, int halo_y, int * periods);
+void mean_position(double * p_real, double * p_imag, double delta_x, double delta_y, int grid_origin_x, int grid_origin_y, double *results,
+                       double norm2, int inner_start_x, int start_x, int inner_end_x, int end_x, int inner_start_y, int start_y, int inner_end_y, int end_y);
+void mean_momentum(double * p_real, double * p_imag, double delta_x, double delta_y, double *results,
+                   double norm2, int inner_start_x, int start_x, int inner_end_x, int end_x, int inner_start_y, int start_y, int inner_end_y, int end_y);
 void get_wave_function_phase(double * phase, double * p_real, double * p_imag, int inner_start_x, int start_x, int inner_end_x, int end_x, int inner_start_y, int start_y, int inner_end_y, int end_y);
 void get_wave_function_density(double * density, double * p_real, double * p_imag, int inner_start_x, int start_x, int inner_end_x, int end_x, int inner_start_y, int start_y, int inner_end_y, int end_y);
 #endif
