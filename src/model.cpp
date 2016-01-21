253a8b8cc1e6ce5a04b5ac33bac107ef2928db80
Peter Wittek <peterwittek@users.noreply.github.com>
2016-01-21 11:36:25 +0100

    OpenMP race conditions resolved in State class

--- src/model.cpp
+++ src/model.cpp
@@ -294,18 +294,17 @@ void State::calculate_expected_values(void) {
                          param_px = - 1. / grid->delta_x,
                          param_py = 1. / grid->delta_y;
 
-    complex<double> psi_up, psi_down, psi_center, psi_left, psi_right;
-    complex<double> psi_up_up, psi_down_down, psi_left_left, psi_right_right;
-    complex<double> const_1 = -1./12., const_2 = 4./3., const_3 = -2.5;
-    complex<double> derivate1_1 = 1./6., derivate1_2 = - 1., derivate1_3 = 0.5, derivate1_4 = 1./3.;
-
-    int y = grid->inner_start_y;
 #ifndef HAVE_MPI
 #pragma omp parallel for reduction(+:sum_norm2,sum_x_mean,sum_y_mean,sum_xx_mean,sum_yy_mean,sum_px_mean,sum_py_mean,sum_pxpx_mean,sum_pypy_mean)
 #endif
-    for (int i = grid->inner_start_y - grid->start_y; i < grid->inner_end_y - grid->start_y; i++) {
-    	int x = grid->inner_start_x;
-        for (int j = grid->inner_start_x - grid->start_x; j < grid->inner_end_x - grid->start_x; j++) {
+    for (int i=grid->inner_start_y - grid->start_y; i < grid->inner_end_y - grid->start_y; ++i) {
+        int y=grid->inner_start_y + i - (grid->inner_start_y - grid->start_y);
+        complex<double> psi_up, psi_down, psi_center, psi_left, psi_right;
+        complex<double> psi_up_up, psi_down_down, psi_left_left, psi_right_right;
+        complex<double> const_1 = -1./12., const_2 = 4./3., const_3 = -2.5;
+        complex<double> derivate1_1 = 1./6., derivate1_2 = - 1., derivate1_3 = 0.5, derivate1_4 = 1./3.;
+        int x = grid->inner_start_x;
+        for (int j = grid->inner_start_x - grid->start_x; j < grid->inner_end_x - grid->start_x; ++j) {
             
             psi_center = complex<double> (p_real[i * tile_width + j], p_imag[i * tile_width + j]);
             sum_norm2 += real(conj(psi_center) * psi_center);
@@ -341,9 +340,8 @@ void State::calculate_expected_values(void) {
                 sum_pxpx_mean += real(conj(psi_center) * (const_1*psi_right_right + const_2*psi_right + const_2*psi_left + const_1*psi_left_left + const_3*psi_center));
                 sum_pypy_mean += real(conj(psi_center) * (const_1*psi_down_down + const_2*psi_down + const_2*psi_up + const_1*psi_up_up + const_3*psi_center));
             }
-            x++;
+            ++x;
         }
-        y++;
     }
     norm2 = sum_norm2;
     mean_X = sum_x_mean;
