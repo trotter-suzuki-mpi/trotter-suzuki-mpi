0874fb062fca14428a43218349a3b32c6978dd64
Lucacalderaro <calderaroluca@gmail.com>
2016-03-05 23:45:57 +0100

    Vortex_position function now detects when a vortex is not present and, in that case, returns Nan

--- src/Python/trottersuzuki/tools.py
+++ src/Python/trottersuzuki/tools.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 def vortex_position(grid, state, approx_cloud_radius=0.):
     """
@@ -63,7 +64,46 @@ def vortex_position(grid, state, approx_cloud_radius=0.):
                 grad_y = (up-dw)/(2.*delta_y)
                 norm_grad[idy, idx] = np.sqrt(grad_x**2 + grad_y**2)
     
-    max_norm = np.nanmax(norm_grad)
+    max_norm = np.nanmax(norm_grad)  # Find max value in norm_grad
+    coord = np.transpose(np.where(norm_grad==max_norm))[0]
+    
+    # Check that the phase has a single discontinuity around the candidate vortex position
+    def position(index, side):
+        index = int(index)
+        side = int(side)
+        idx = int(math.fmod(index,4*side))  # position is periodic of period 4*radius
+        quad = idx / side
+        rest = int(math.fmod(idx,side))
+        if quad == 0:
+            x = - (side / 2 + 1)
+            y = rest - side / 2
+        if quad == 1:
+            y = (side / 2 + 1)
+            x = rest - side / 2
+        if quad == 2:
+            x = (side / 2 + 1)
+            y = - (rest - side / 2)
+        if quad == 3:
+            y = - (side / 2 + 1)
+            x = - (rest - side / 2)
+        return np.array([y, x])
+        
+    side = 8 # must be an even number
+    vortex = 0
+    for i in range(0, side*4):  # Count the number of discontinuity in the phase pattern around the candidate vortex position
+        pos1 = coord + position(i, side)
+        pos2 = coord + position(i+1, side)
+        if pos1[0] < 0 or pos1[0] > grid.dim_x or pos1[1] < 0 or pos1[1] > grid.dim_x:
+            if pos2[0] < 0 or pos2[0] > grid.dim_x or pos2[1] < 0 or pos2[1] > grid.dim_x:
+                break
+        phase1 = matrix[pos1[0], pos1[1]]
+        phase2 = matrix[pos2[0], pos2[1]]
+        if np.abs(phase1 - phase2) >= np.pi * 1.7: 
+            vortex += 1
+    
+    if vortex != 1:  # around the vortex there must be a single discontinuity in the phase
+        return np.array([np.nan, np.nan])
+    
     coord_x = []
     coord_y = []
     for idy in range(1,grid.dim_y-1):
@@ -76,5 +116,5 @@ def vortex_position(grid, state, approx_cloud_radius=0.):
     for i in range(0, len(coord_x)):
         coords[1] += coord_y[i] / float(len(coord_x))
         coords[0] += coord_x[i] / float(len(coord_x))
-
+        
     return coords
