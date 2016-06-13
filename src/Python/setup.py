4480befb41a86b585d265fe6a1e6b78bef959513
Peter Wittek <peterwittek@users.noreply.github.com>
2016-06-13 14:27:24 +0200

    Preparing for release 1.5.5

--- src/Python/setup.py
+++ src/Python/setup.py
@@ -145,7 +145,7 @@ else:
 
 
 setup(name='trottersuzuki',
-      version='1.5.4',
+      version='1.5.5',
       license='GPL3',
       author="Peter Wittek, Luca Calderaro",
       author_email='peterwittek@users.noreply.github.com',
